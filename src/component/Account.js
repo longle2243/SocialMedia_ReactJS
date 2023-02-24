df5497febb2773ff1f2ddc35792b3f24707b7d78
Quang Long <95632186+longle2243@users.noreply.github.com>
2023-02-24 15:59:22 +0900

    Update Account.js

--- src/component/Account.js
+++ src/component/Account.js
@@ -1,7 +1,7 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Box } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
 
 import React, { Component } from 'react'
@@ -13,7 +13,7 @@ export default class Account extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/photos")
+    fetch("https://dummyjson.com/products/category/smartphones")
       .then(res => res.json())
       .then(data => {
         this.setState({ data: data });
@@ -21,19 +21,51 @@ export default class Account extends Component {
   }
 
   render() {
+    this.state.data.products && console.log(this.state.data.products[0])
     return (
       <div>
-        {this.state.data.map(todo => (
-          <Card sx={{ maxWidth: 345, m: "auto", mt: 5, boxShadow: 5 }}>
-            <CardMedia sx={{ height: 240 }} image={todo.url} />
-            <CardActionArea>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">{todo.id}</Typography>
-                <Typography variant="body2" color="text.secondary" key={todo.id}>{todo.title}</Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+        {
+          this.state.data.products ?
+            <div>
+              {this.state.data.products.map(todo => (
+                <div key={todo.id}>
+                  {todo.category === "smartphones" && (
+                    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="row" sx={{ maxWidth: 1000, m:"auto"}}>
+                      <Card sx={{ maxWidth: 345, m: "auto", mt: 5, boxShadow: 5 }}>
+                        <CardMedia sx={{ height: 240, width:300 }} image={todo.thumbnail} />
+                        <CardActionArea>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">{todo.id}</Typography>
+                            <Typography variant="body2" color="text.secondary">{todo.title}</Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+
+                      <Card sx={{ maxWidth: 345, m: "auto", mt: 5, boxShadow: 5 }}>
+                        <CardMedia sx={{ height: 240, width:300 }} image={todo.thumbnail} />
+                        <CardActionArea>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">{todo.id}</Typography>
+                            <Typography variant="body2" color="text.secondary">{todo.title}</Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+
+                      <Card sx={{ maxWidth: 345, m: "auto", mt: 5, boxShadow: 5 }}>
+                        <CardMedia sx={{ height: 240, width:300 }} image={todo.thumbnail} />
+                        <CardActionArea>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">{todo.id}</Typography>
+                            <Typography variant="body2" color="text.secondary">{todo.title}</Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Box>
+                  )}
+                </div>
+              ))}
+            </div> : <p>Loading ...</p>
+        }
       </div>
     )
   }
